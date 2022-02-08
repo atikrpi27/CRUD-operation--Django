716474b7862bec0836395cd00d7c8911d280caa5
atikrpi27 <atik.rpi27@gmail.com>
2022-02-09 00:41:41 +0600

    Add Artist profile Info and list of album

--- crud/views.py
+++ crud/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Musician, Album
 from . import form
+from django.db.models import Avg
 # Create your views here.
 
 def index(request):
@@ -37,5 +38,8 @@ def album(request):
 
 def album_list(request,artist_id):
     artist_info = Musician.objects.get(pk=artist_id)
-    diction = {'title' : 'Album List: ', 'artist_info': artist_info}
+    album_list = Album.objects.filter(artist=artist_id).order_by('name')
+    artist_ratting = Album.objects.filter(artist=artist_id).aggregate(Avg('num_stars'))
+
+    diction = {'title' : 'Album List: ', 'artist_info': artist_info, 'album_list': album_list, 'artist_ratting':artist_ratting}
     return render(request, 'album_list.html', diction)
